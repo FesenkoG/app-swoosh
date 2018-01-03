7b266dbbdd538379ba67bddba8f42f492d36a3fc
Georgy <fesenko.g@gmail.com>
2018-01-03 12:47:21 +0300

    well, almost done

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Георгий Фесенко on 03.01.2018.
+//  Copyright © 2018 Georgy. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
